20dbf841b390b4199e520779ad732d53e34ea6df
griffin mbp <griffin.reed3@gmail.com>
2020-08-27 18:44:20 +0200

    This should have been commited a long while ago

---
+++ srcs/wordpress/srcs/wpinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cd /www
+wp core is-installed
+if [ $? == 1 ]; then
+	echo "wp core is-installed has exit status of 1"
+    wp core download
+    wp core install --url=wordpress/ --path=/www --title="Wordpress" --admin_user="admin" --admin_password="pass" --admin_email=admin@system.com --skip-email
+    wp term create category Test
+    wp post create --post_author=admin --post_category="Test" --post-title="Test Title" --post-content="yeet" --post_excerpt=tag --post_status=publish | awk '{gsub(/[.]/, ""); print $4}' > /tmp/postid
+	wp user create "bob" "writer@example.com" --role="writer" --user_pass="pass"
+	wp user create "fran" "dude@example.com" --role="dude" --user_pass="pass"
+	wp user create "bruce" "man@example.com" --role="man" --user_pass="pass"
+
+	wp theme install twentyten
+	wp theme install twentytwenty
+	wp theme activate twentyten
+	wp plugin install woocommerce
+else
+	echo "WordPress has already been installed in /www"
+fi
---
+++ startapps.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#------------------------------------COLORS------------------------------------#
+RED=$'\e[1;31m'
+GREEN=$'\e[1;32m'
+YELLOW=$'\e[1;33m'
+BLUE=$'\e[1;34m'
+MAGENTA=$'\e[1;35m'
+CYAN=$'\e[1;36m'
+END=$'\e[0m'
+
+#------------------------------------FUNCTIONS------------------------------------#
+
+# $1 = name, $2 = docker-location, $3 = yml-location, $4 = DEBUG
+start_app () {
+	printf "$1: "
+	if [ $4 -eq 0 ]
+	then
+		docker build -t $1 $2 > /dev/null 2>>errlog.txt && kubectl apply -f $3 > /dev/null 2>>errlog.txt
+	else
+		docker build -t $1 $2 && kubectl apply -f $3
+	fi
+    RET=$?
+	if [ $RET -eq 1 ]
+	then
+		echo "[${RED}NO${END}]"
+	else
+		echo "[${GREEN}OK${END}]"
+	fi
+}
+
+#------------------------------------CLEANUP------------------------------------#
+
+#rm -rf ~/.minikube
+#mkdir -p ~/goinfre/.minikube
+#ln -s ~/goinfre/.minikube ~/.minikube
+
+:> errlog.txt
+:> log.log
+#sh cleanup.sh >> log.log 2>> /dev/null
+
+#----------------------------------BUILD AND DEPLOY----------------------------------#
+
+eval $(minikube docker-env)
+export MINIKUBE_IP=$(minikube ip)
+
+DEUBUG=""
+if [ $# -eq 1 ]
+then
+	DEBUG=1
+else
+	DEBUG=0
+fi
+
+#docker build -t nginx_alpine ./srcss/nginx
+
+kubectl apply -f ./srcs/metallb/metallb-config.yml
+start_app "nginx" "./srcs/nginx" "./srcs/nginx.yml" "$DEBUG"
+start_app "ftps" "./srcs/ftps" "./srcs/ftps.yml" "$DEBUG"
+start_app "mysql" "./srcs/mysql" "./srcs/mysql/mysql.yml"
+start_app "wordpress" "./srcs/wordpress" "./srcs/wordpress/wordpress.yml"
+start_app "phpmyadmin" "./srcs/phpmyadmin" "./srcs/phpmyadmin/phpmyadmin.yml"
